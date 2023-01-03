754bae549ffcdbd060e5f2affd397eb22c5a2223
임우석 <85557627+WSLimmm@users.noreply.github.com>
2023-01-03 23:07:03 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, sum = 0;
+	cin >> n;
+
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <=i; j++)
+		{
+			sum += i + j;
+		}
+	}
+	cout << sum;
+
+	return 0;
+}
