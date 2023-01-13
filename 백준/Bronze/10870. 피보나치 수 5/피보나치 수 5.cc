3dce1831e2bce9f5dd367ae4c0a9550c1fa14f9b
임우석 <85557627+WSLimmm@users.noreply.github.com>
2023-01-13 22:44:23 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+const int COUNT = 21;
+
+int main()
+{
+	int n;
+	long long arr[COUNT] = { };
+	cin >> n;
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		if (i == 0)
+			arr[i] = 0;
+		else if (i == 1)
+			arr[i] = 1;
+		else if (i >= 2)
+			arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	cout << arr[n] << endl;
+
+	return 0;
+}
